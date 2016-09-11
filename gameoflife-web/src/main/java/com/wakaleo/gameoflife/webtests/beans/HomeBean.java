8ed8a0530e51186fcd3d695ba9c050c0f26d3692
gijshendrikx <gijshendrikx1990@gmail.com>
2016-09-11 17:19:59 +0200

    sonar test
    Test of sonar slechte code detecteert

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/beans/HomeBean.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/beans/HomeBean.java
@@ -5,6 +5,7 @@
  */
 package com.wakaleo.gameoflife.webtests.beans;
 
+import java.util.Date;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
@@ -17,6 +18,18 @@ import javax.inject.Named;
 @RequestScoped
 public class HomeBean {
     public String index() {
+        String datum = new Date().toString();
+        
+        if(datum=="test"){
+            //dit is onzin
+        } else if(datum=="blabla"){
+            //nog meer onzin
+        } else if (datum.equals("tjaaa")){
+            //dit is de laatste onzin
+        } else if (datum == "thaaa"){
+            //asdasdsad
+        }
+        
         return "home";
     }
     
